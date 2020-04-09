6c5879a62dbef852cafb0128a1632d6387a39247
swapnil159 <swapnilmahajan159@gmail.com>
2020-04-09 17:33:30 +0530

    Updates

---
+++ precache-manifest.41f4e20625c26c0201fe54ac655cc946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069faa358ea3b0fd059fe6b5a3bf524e",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "e215c0c9a6524367b4c7",
+    "url": "/Tic-Tac-Toe/static/css/main.dcd61942.chunk.css"
+  },
+  {
+    "revision": "dedce77480305b376a31",
+    "url": "/Tic-Tac-Toe/static/js/2.65fc5f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.65fc5f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e215c0c9a6524367b4c7",
+    "url": "/Tic-Tac-Toe/static/js/main.423eb400.chunk.js"
+  },
+  {
+    "revision": "0529e855b5e18e30e2f9",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.0c1f8c58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
